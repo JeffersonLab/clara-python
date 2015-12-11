7c81791da9d79cd74cc2ad12559ff6fd089710c1
Ricardo Oyarzun <royarzun@gmail.com>
2015-12-11 13:27:45 -0300

    CConstants: Add new constants

--- clara/util/CConstants.py
+++ clara/util/CConstants.py
@@ -19,37 +19,46 @@
 # SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #
 
+
 class CConstants:
-    
-    def __init__(self):
-        pass
-    
+
     DPE = "dpe"
+    START_DPE = "startDpe"
+    STOP_DPE = "stopDpe"
     DPE_UP = "dpeIsUp"
     DPE_DOWN = "dpeIsDown"
     DPE_PING = "dpePing"
     DPE_EXIT = "dpeExit"
+    DPE_ALIVE = "dpeAlive"
+    LIST_DPES = "listDpes"
+
+    CONTAINER = "container"
     START_CONTAINER = "startContainer"
+    STOP_CONTAINER = "stopContainer"
+    CONTAINER_UP = "containerIsUp"
+    CONTAINER_DOWN = "containerIsDown"
     REMOVE_CONTAINER = "removeContainer"
     LIST_CONTAINERS = "listContainers"
 
-    CONTAINER = "container"
-    CONTAINER_UP = "containerIsUp"
-    CONTAINER_DOWN = "containerIsDown"
+    SERVICE = "service"
+    START_SERVICE = "startService"
+    STOP_SERVICE = "stopService"
     DEPLOY_SERVICE = "deployService"
     REMOVE_SERVICE = "removeService"
-    RUN_SERVICE = "runService"
-
-    SERVICE = "service"
     SERVICE_UP = "serviceIsUp"
     SERVICE_DOWN = "serviceIsDown"
     SERVICE_REPORT_DONE = "serviceReportDone"
     SERVICE_REPORT_DATA = "serviceReportData"
+    RUN_SERVICE = "runService"
+    LIST_SERVICES = "listServices"
 
+    SHARED_MEMORY_KEY = "clara/shmkey"
     ALIVE = "alive"
-    
-    LANG_SEP = "_"
+
     TOPIC_SEP = ":"
     DATA_SEP = "?"
+    LANG_SEP = "_"
 
     BENCHMARK = 10000
+
+    UNDEFINED = "undefined"
