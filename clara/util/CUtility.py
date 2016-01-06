531a59c74744a2b8ff9e305ad105120ce04f8fc8
Ricardo Oyarzun <royarzun@gmail.com>
2016-01-05 20:55:27 -0600

    Add remove_first method to CUtility

--- clara/util/CUtility.py
+++ clara/util/CUtility.py
@@ -22,13 +22,8 @@
 
 from xmsg.core.xMsgUtil import xMsgUtil
 
-__author__ = 'gurjyan'
-
 
 class CUtility(object):
-
-    def __init__(self):
-        pass
 
     @staticmethod
     def form_canonical_name(host, container, engine_name):
@@ -37,9 +32,20 @@ class CUtility(object):
         Constructs and returns CLARA specified canonical name.
         For e.g. service name convention, i.e. host:container:engine
 
-        :param host: DPE host IP address
-        :param container: Clara service container name
-        :param engine_name: Clara service engine name
-        :return: canonical name of the Clara service
+        Args:
+            host (String): DPE host IP address
+            container (String): Clara service container name
+            engine_name (String): Clara service engine name
+
+        Returns:
+            canonical name of the Clara service
         """
-        return xMsgUtil.host_to_ip(host) + ":" + str(container) + ":" + str(engine_name)
+        return "%s:%s:%s" % (xMsgUtil.host_to_ip(host),
+                             str(container), str(engine_name))
+
+    @staticmethod
+    def remove_first(self, input_string, first_character):
+        if input_string.startswith(first_character):
+            return input_string[1:]
+        else:
+            return input_string
