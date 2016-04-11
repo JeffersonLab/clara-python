84a8611668876f49fbd39cfbe5de4794ec050efa
Ricardo Oyarzun <royarzun@gmail.com>
2016-04-11 19:41:14 -0300

    clara.sys: Add working Dpe and Container (xmsg-2.3.1)

--- clara/sys/Container.py
+++ clara/sys/Container.py
@@ -1,72 +1,59 @@
 # coding=utf-8
-#
-# Copyright (C) 2015. Jefferson Lab, Clara framework (JLAB). All Rights Reserved.
-# Permission to use, copy, modify, and distribute this software and its
-# documentation for educational, research, and not-for-profit purposes,
-# without fee and without a signed licensing agreement.
-#
-# Author Ricardo Oyarzun
-# Department of Experimental Nuclear Physics, Jefferson Lab.
-#
-# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
-# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
-# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
-# OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
-# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-#
 
+from xmsg.net.xMsgAddress import ProxyAddress, RegAddress
 from clara.base.ClaraBase import ClaraBase
-from clara.name.ServiceName import ServiceName
+from clara.base.ClaraNames import ServiceName
 from clara.sys.Service import Service
 from clara.util.ClaraLogger import ClaraLogger
 
 
 class Container(ClaraBase):
-    my_services = {}
+    my_services = dict()
 
-    def __init__(self, name, local_address, frontend_address):
-        super(Container, self).__init__(name,
+    def __init__(self, container_name, local_address, frontend_address):
+        super(Container, self).__init__(container_name.canonical_name(),
                                         local_address.host,
                                         frontend_address.host,
                                         local_address.pub_port,
                                         frontend_address.port)
+        self.container_name = container_name
         self.logger = ClaraLogger(repr(self))
-        self.logger.log_info("started: " + self.myname)
+        self.logger.log_info("container deployed")
 
     def __repr__(self):
         return str("Container:%s" % self.myname)
 
     def exit(self):
         self.__remove_services()
-        self.logger.log_info("stopped: " + self.myname)
+        self.logger.log_info("container stopped")
 
     def add_service(self, engine_name, engine_class, service_pool_size,
                     initial_state):
-        service_name = ServiceName(self.myname, engine_name)
+        service_name = ServiceName(self.container_name, engine_name)
 
-        if service_name.name in self.my_services.keys():
-            self.logger.log_warning("Service %s already exists. No new"
+        if service_name.canonical_name() in self.my_services:
+            self.logger.log_warning("Service %s already exists. No new "
                                     "service is deployed" % str(service_name))
-
         else:
-            service = Service(service_name.canonical_name(),
-                              engine_class, engine_name,
-                              service_pool_size, self.default_registrar_address,
-                              initial_state, self.default_proxy_address)
+            try:
+                service = Service(service_name,
+                                  engine_class,
+                                  engine_name,
+                                  service_pool_size,
+                                  initial_state,
+                                  ProxyAddress(),
+                                  RegAddress())
+                self.my_services[service_name.canonical_name()] = service
 
-            self.my_services[engine_name] = service
-            self.logger.log_info("service deployed: " + engine_name)
+            except Exception as e:
+                self.logger.log_exception("%s: %s" % (str(service_name), e))
+                raise e
 
     def remove_service(self, service_name):
-        if service_name in self.my_services.keys():
+        if service_name in self.my_services:
             service = self.my_services.pop(service_name)
             service.exit()
 
     def __remove_services(self):
-        for service_key in self.my_services.keys():
-            self.my_services[service_key].exit()
+        for service in self.my_services:
+            service.exit()
--- clara/sys/Dpe.py
+++ clara/sys/Dpe.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python
 # coding=utf-8
-#
-# Copyright (C) 2015. Jefferson Lab, Clara framework (JLAB). All Rights Reserved.
-# Permission to use, copy, modify, and distribute this software and its
-# documentation for educational, research, and not-for-profit purposes,
-# without fee and without a signed licensing agreement.
-#
-# Author Ricardo Oyarzun
-# Department of Experimental Nuclear Physics, Jefferson Lab.
-#
-# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
-# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
-# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
-# OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
-# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-#
 
 import argparse
 from xmsg.core.xMsgUtil import xMsgUtil
 from xmsg.core.xMsgCallBack import xMsgCallBack
 from xmsg.core.xMsgConstants import xMsgConstants
+from xmsg.net.xMsgAddress import ProxyAddress, RegAddress
 
 from clara.base.ClaraBase import ClaraBase
 from clara.base.ClaraLang import ClaraLang
+from clara.base.ClaraNames import DpeName, ContainerName
 from clara.base.ClaraUtils import ClaraUtils
-from clara.name.DpeName import DpeName
 from clara.sys.Container import Container
 from clara.util.CConstants import CConstants
 from clara.util.ClaraLogger import ClaraLogger
@@ -38,7 +19,7 @@ from clara.util.RequestParser import RequestParser
 
 class Dpe(ClaraBase):
 
-    my_containers = {}
+    my_containers = dict()
     subscription_handler = None
 
     def __init__(self, local_address, frontend_address,
@@ -56,16 +37,13 @@ class Dpe(ClaraBase):
         self.logger = ClaraLogger(repr(self))
         self.print_logo()
 
+        topic = ClaraUtils.build_topic(CConstants.DPE, self.myname)
         try:
-            topic = ClaraUtils.build_topic(CConstants.DPE, self.myname)
-            self.subscription_handler = self.listen(topic,
-                                                    self.node_connection,
-                                                    DpeCallBack(self))
+            self.subscription_handler = self.listen(topic, _DpeCallBack(self))
             xMsgUtil.keep_alive()
 
         except KeyboardInterrupt:
             self.exit()
-            return
 
     def __repr__(self):
         return str("Dpe:%s" % self.myname)
@@ -73,9 +51,9 @@ class Dpe(ClaraBase):
     def exit(self):
         self.logger.log_info("Gracefully quitting the dpe...")
         for container in self.my_containers:
-            container.stop()
+            container.exit()
             container.destroy()
-        self.unsubscribe(self.subscription_handler)
+        self.stop_listening(self.subscription_handler)
 
     def print_logo(self):
         print "================================================"
@@ -97,65 +75,76 @@ class Dpe(ClaraBase):
 
     def start_container(self, parser):
         container_name = parser.next_string()
-        if container_name in self.my_containers:
-            self.logger.log_warning("Container " + str(container_name) +
-                                    " already exists. No new container is created")
-        else:
-            name = ClaraUtils.form_container_name(self.myname, container_name)
-            container = Container(name, self.default_proxy_address,
-                                  self.default_registrar_address)
-            self.my_containers[container_name] = container
+        try:
+            if container_name in self.my_containers:
+                self.logger.log_warning("Container " + str(container_name) +
+                                        " already exists. No new container is"
+                                        " created")
+            else:
+                container = Container(ContainerName(self.dpe_name,
+                                                    container_name),
+                                      ProxyAddress(), RegAddress())
+                self.my_containers[container_name] = container
+
+        except Exception as e:
+            self.logger.log_exception(e.message)
+            raise e
 
     def stop_container(self, parser):
         container_name = parser.next_string()
-        if container_name in self.my_containers.keys():
+        if container_name in self.my_containers:
             container = self.my_containers.pop(container_name)
             container.exit()
 
     def start_service(self, parser):
-        container_name = parser.next_string()
-        engine_name = parser.next_string()
-        engine_class = parser.next_string()
-        pool_size = parser.next_integer()
-        description = parser.next_string()
-        initial_state = parser.next_string()
+        try:
+            service_name = parser.next_string()
+            container_name = ClaraUtils.get_container_name(service_name)
+            engine_name = parser.next_string()
+            engine_class = parser.next_string()
+            pool_size = parser.next_integer()
+            description = parser.next_string()
+            initial_state = parser.next_string()
 
-        if container_name in self.my_containers:
-            self.my_containers[container_name].add_service(engine_name,
-                                                           engine_class,
-                                                           pool_size,
-                                                           description,
-                                                           initial_state)
-        else:
-            raise Exception("Could not stop service %s: missing container " % engine_name)
+            if container_name in self.my_containers:
+                self.my_containers[container_name].add_service(engine_name,
+                                                               engine_class,
+                                                               pool_size,
+                                                               initial_state)
+
+        except Exception as e:
+            self.logger.log_exception(e.message)
+            raise e
 
     def stop_service(self, parser):
         container_name = parser.next_string()
         engine_name = parser.next_string()
         service_name = ClaraUtils.form_service_name(container_name,
                                                     engine_name)
-        if container_name in self.my_containers.keys():
+        if container_name in self.my_containers:
             try:
                 self.my_containers[container_name].remove_service(service_name)
 
             except Exception as e:
                 raise Exception("Could not stop service %s: %s "
                                 % (service_name, e))
+
         else:
-            raise Exception("Could not stop service %s: missing container " % service_name)
+            raise Exception("Could not stop service %s: missing container "
+                            % service_name)
 
 
-class DpeCallBack(xMsgCallBack):
+class _DpeCallBack(xMsgCallBack):
 
     def __init__(self, dpe):
-        super(DpeCallBack, self).__init__()
+        super(_DpeCallBack, self).__init__()
         self.dpe = dpe
 
     def callback(self, msg):
         try:
             parser = RequestParser.build_from_message(msg)
             cmd = parser.next_string()
-            self.dpe.logger.log_info("DPE received: " + cmd)
+            self.dpe.logger.log_info("received: %s" % cmd)
 
             if cmd == CConstants.STOP_DPE:
                 self.dpe.exit()
