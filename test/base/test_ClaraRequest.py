41a263e9022beb1d1dfbb9fb76df7f0a4ab3633b
Ricardo Oyarzun <royarzun@gmail.com>
2016-04-11 19:42:36 -0300

    ClaraRequest.tests: Update references to ClaraNames

--- test/base/test_ClaraRequest.py
+++ test/base/test_ClaraRequest.py
@@ -1,31 +1,9 @@
-#
-# Copyright (C) 2015. Jefferson Lab, Clara framework (JLAB). All Rights Reserved.
-# Permission to use, copy, modify, and distribute this software and its
-# documentation for educational, research, and not-for-profit purposes,
-# without fee and without a signed licensing agreement.
-#
-# Author Ricardo Oyarzun
-# Department of Experimental Nuclear Physics, Jefferson Lab.
-#
-# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
-# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
-# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
-# OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
-# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-#
+# coding=utf-8
 
 import unittest
 from clara.base.ClaraBase import ClaraBase
+from clara.base.ClaraNames import DpeName, ContainerName, ServiceName
 from clara.base.ClaraRequest import DeployContainerRequest, DeployServiceRequest
-from clara.name.ContainerName import ContainerName
-from clara.name.ServiceName import ServiceName
-from clara.name.DpeName import DpeName
-
 
 
 class TestClaraRequest(unittest.TestCase):
