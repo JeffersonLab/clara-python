a49b5c9c6f7ca82a9397ecb50d670d53616a1a41
Ricardo Oyarzun <royarzun@gmail.com>
2016-04-12 14:28:51 -0300

    clara.sys.Service: Remake clara service implementation
    Clara Service class inherits directly from ClaraBase,
    implementation has been redone from the scratch to
    simplify the code.

--- clara/sys/Service.py
+++ clara/sys/Service.py
@@ -1,244 +1,139 @@
-#!/usr/bin/env python
-#
-# Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
-# Permission to use, copy, modify, and distribute this software and its
-# documentation for educational, research, and not-for-profit purposes,
-# without fee and without a signed licensing agreement.
-#
-# Author Vardan Gyurjyan
-# Department of Experimental Nuclear Physics, Jefferson Lab.
-#
-# IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
-# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
-# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
-# OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
-# HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
-# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-#
+# coding=utf-8
 
-import sys
-import threading
-from copy import deepcopy
-from xmsg.core.xMsgConstants import xMsgConstants
-from xmsg.core.xMsgUtil import xMsgUtil
-from xmsg.data import xMsgData_pb2
+from xmsg.core.xMsgCallBack import xMsgCallBack
+from xmsg.data.xMsgMeta_pb2 import xMsgMeta
 
-from clara.base.ServiceBase import ServiceBase
-from clara.sys.ServiceExecutor import ServiceExecutor
-from clara.util.CUtility import CUtility
+from clara.base.ClaraBase import ClaraBase
+from clara.base.ClaraUtils import ClaraUtils
+from clara.util.ClaraLogger import ClaraLogger
+from clara.util.CConstants import CConstants
+from clara.util.RequestParser import RequestParser
 
 
-__author__ = 'gurjyan'
-
-
-class Service(ServiceBase):
-    """
-    Service container/broker class. This creates ServiceMP object pool.
-    Handles subscriptions and callbacks.
-    Calls service (service executor) object's run method in a separate thread.
+class Service(ClaraBase):
+    """Service container/broker class. This creates ServiceMP object pool.
+    Handles subscriptions and callbacks. Calls service (service executor)
+    object's run method in a separate thread.
 
     Attributes:
         engine_class (String): the name of the python class containing
             service engine class
-        container (String): the name of service container, for logical
-            grouping of services.
-        engine_class_name (String): the name of the service engine python class
+        engine_name (String): the name of the service engine python class
             within the engine_container_class
-        p_size (Int): Service object pool size, i.e. number of parallel services
+        pool_size (int): Service object pool size, i.e. number of parallel
+            services
     """
+    subscription_handler = None
 
-    def __init__(self, engine_class, container, engine_class_name, p_size):
-        # local copy of the previous composition request,
-        # obtained from previously received transient data
-        self.p_composition = xMsgConstants.UNDEFINED
+    def __init__(self, name, engine_class, engine_name, pool_size,
+                 initial_state, local_address, frontend_address):
+        super(Service, self).__init__(name.canonical_name(),
+                                      local_address.host,
+                                      frontend_address.host,
+                                      local_address.pub_port,
+                                      frontend_address.port)
 
-        # the dynamic (updated for every request) repository/map
-        # of input-linked service names that are required to be logically
-        # AND-ed
-        self.dyn_in_name_list = dict()
-
-        # the dynamic ( updated for every request) repository/map
-        # of input-linked service data that are required to be logically
-        # AND-ed
-        self.dyn_in_data_list = dict()
-
-        # local map of input-linked services for every
-        # composition in multi-composition application.
-        # Note: by design compositions are separated by ";"
-        self.in_links = dict()
-
-        # local boolean indicating that service has multiple
-        # inputs and they are required to be logically ANDed
-        self.log_and_request = False
-
+        self.logger = ClaraLogger(repr(self))
         # user provided engine class container class name
         self.engine_class = engine_class
-
-        # the name of the CLARA container, used for logical
-        # separation of services deploying the service engine
-        self.container = container
-
         # actual engine class name
-        self.engine_class_name = engine_class_name
-
-        ServiceBase.object_pool_size = int(p_size)
-
-        # Defines this service canonical name
-        service_name = CUtility.form_canonical_name(xMsgUtil.get_local_ip(),
-                                                    self.container,
-                                                    self.engine_class_name)
-        ServiceBase.__init__(self, service_name)
-
+        self.engine_name = engine_name
+        # initial state of the service
+        self.initial_state = initial_state
+        # pool size
+        self.pool_size = pool_size
         # Create service executor objects and fill the pool
-        for _ in range(ServiceBase.object_pool_size):
-            instance = ServiceExecutor(self.engine_class,
-                                       self.container,
-                                       self.engine_class_name)
+        self.available_object_pool = dict()
 
-            ServiceBase.available_object_pool.put(instance)
+        self.engine_pool = []
 
         # Dynamically loads service engine class
-        engine_class = self._for_name(self.engine_class,
-                                      self.engine_class_name)
+        engine_class = self.__load_engine(self.engine_class, self.engine_name)
         self.engine_object = engine_class()
+
+        self.logger.log_info("deploying service...")
 
         # Get description defined in the service engine
         self.description = self.engine_object.get_description()
 
-        print "Registering with registration services..."
+        try:
+            # Subscribe messages addressed to this service container
+            topic = ClaraUtils.build_topic(CConstants.SERVICE, self.myname)
+            self.subscription_handler = self.listen(topic, _ServiceCallBack(self))
+            self.logger.log_info("service deployed")
 
-        # registration
-        self.registerSubscriber(self.name,
-                                xMsgUtil.get_local_ip(),
-                                self.container,
-                                self.engine_class_name,
-                                self.description)
+        except Exception as e:
+            self.logger.log_exception(str(e))
+            raise e
 
-        # Subscribe messages addressed to this service container
-        self.receive(self.name, self.call_back, False)
+    def __repr__(self):
+        return str("Service:%s" % self.myname)
 
-        xMsgUtil.keep_alive()
+    def configure(self, msg):
+        while True:
+            for engine in self.engine_pool:
+                if engine.try_acquire():
+                    try:
+                        engine.configure(msg)
+                    except Exception as e:
+                        self.logger.log_exception(e.message)
+                    finally:
+                        engine.release()
 
-    def call_back(self, tr_data):
-        """ Service callback. This calls engine passing received data to it
+                return
 
-        Params:
-            tr_data
-        """
-        self.start_service(tr_data)
+    def execute(self, msg):
+        pass
 
-    def start_service(self, tr_object):
+    def __load_engine(self, module_name, engine_name):
+        loaded_module = __import__(module_name, fromlist=[engine_name])
+        try:
+            self.service_object = getattr(loaded_module, engine_name)
 
-        composition = tr_object.composition
+        except ImportError as e:
+            self.logger.log_exception(str(e))
+            raise e
 
-        # check to see if this is a configure request
-        if tr_object.action == xMsgData_pb2.xMsgData.CONFIGURE:
+        return self.service_object
 
-            # configure all engines in all service objects and return
-            # Note: the successful configuration assumes that all
-            #       objects are in the available pool.
-            for _ in range(ServiceBase.available_object_pool.qsize()):
-                ins = ServiceBase.available_object_pool.get()
-                ins.run(tr_object)
-                ServiceBase.used_object_pool.put(ins)
+    def setup(self, msg):
+        setup = RequestParser.build_from_message(msg)
+        report = setup.next_string()
+        value = setup.next_integer()
 
-            # return all objects to the pool of available objects
-            for _ in range(ServiceBase.used_object_pool.qsize()):
-                ins = ServiceBase.used_object_pool.get()
-                ServiceBase.available_object_pool.put(ins)
-            return
+        if report == CConstants.SERVICE_REPORT_DONE:
+            # TODO: Implement ServiceSysConfig
+            pass
 
-        if tr_object.composition != self.p_composition:
-
-            # This is new routing (composition)  request
-            # clear local input-link dictionary and output-links list
-            self.in_links.clear()
-
-            # store it as previous
-            self.p_composition = tr_object.composition
-
-            # parse the new composition to find input and output
-            # linked service names, but first check to see if we
-            # have multiple parallel compositions (branching)
-            if ";" in tr_object.composition:
-                l_comps = tr_object.composition.split(";")
-                for comp in l_comps:
-                    if self.name in comp:
-                        self.in_links[comp] = []
-
-                        for x in self.parse_in_linked(self.name, comp):
-                            self.in_links[comp].append(x)
-
-            else:
-                self.in_links[tr_object.composition] = \
-                    self.parse_in_linked(self.name, tr_object.composition)
-
-        # define if this service has multiple inputs
-        # and they are required to be logically ANDed
-        log_and_request = self._is_log_and(self.name, composition)
-
-        if log_and_request:
-
-            # is the sender service is part of the input link schema
-            if tr_object.sender in self.in_links.get(composition):
-
-                # is this first time that service reporting?
-                # if so create name map for this composition and
-                # add the senders name to the list, mapped by
-                # the composition string
-                if composition not in self.dyn_in_name_list.keys():
-                    x = [tr_object.sender]
-                    ll = [tr_object]
-                    self.dyn_in_name_list[composition] = x
-                    self.dyn_in_data_list[composition] = ll
-                    print "got the first request from sender = " + tr_object.sender
-
-                # if composition exists add the senders name
-                # to the list if not already added
-                elif tr_object.sender not in self.dyn_in_name_list.get(composition):
-                    self.dyn_in_name_list.get(composition).append(tr_object.sender)
-                    self.dyn_in_data_list.get(composition).append(tr_object)
-                    print "got the request from sender = %s %s" % (tr_object.sender,
-                                                                   str(len(self.dyn_in_data_list.get(composition))))
-
-                # check to see if all inputs for a logical AND are already sent data
-                if len(self.dyn_in_name_list.get(composition)) == len(self.in_links.get(composition)):
-
-                    # deep copy list of input data to this service
-                    g_list = deepcopy(self.dyn_in_data_list.get(composition))
-
-                    # get service object from the object pool and call
-                    # execute method of that object in a separate thread
-                    ins = self.get_object_from_pool()
-
-                    print " Calling service executor = " + ins.name
-                    # execute object method in a separate thread
-                    t = threading.Thread(target=ins.run, args=g_list)
-                    t.daemon = True
-                    t.start()
-
-                    # reset dynamic lists of composition specific lists
-                    self.dyn_in_name_list.get(composition)[:] = []
-                    self.dyn_in_data_list.get(composition)[:] = []
+        elif report == CConstants.SERVICE_REPORT_DATA:
+            # TODO: Implement ServiceSysConfig
+            pass
 
         else:
-            # get service object from the object pool and call
-            # execute method of that object in a separate thread
-            l = [tr_object]
-            ins = self.get_object_from_pool()
-            # execute object method in a separate thread
-            t = threading.Thread(target=ins.run, args=l)
-            t.daemon = True
-            t.start()
+            self.logger.log_error("invalid report request: %s" % str(report))
+
+    def exit(self):
+        self.stop_listening(self.subscription_handler)
+        self.logger.log_info("service stopped")
 
 
-@classmethod
-def main(engine_class, container, engine, object_pool_size):
-    Service(engine_class, container, engine, object_pool_size)
+class _ServiceCallBack(xMsgCallBack):
 
-if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    def __init__(self, service):
+        self.service = service
+
+    def callback(self, msg):
+        try:
+            metadata = xMsgMeta()
+            metadata.MergeFrom(msg.metadata)
+            if metadata.action == 0:
+                self.service.setup(msg)
+
+            elif metadata.action == xMsgMeta.CONFIGURE:
+                self.service.configure(msg)
+
+            else:
+                self.service.execute(msg)
+
+        except Exception as e:
+            self.service.logger.log_exception(str(e))
