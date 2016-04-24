c253fe4ce5d9512f5b44adb13a3483189347c956
Ricardo Oyarzun <royarzun@gmail.com>
2016-04-24 02:07:14 -0300

    clara.sys.ccc: Add Clara Compiler module
    this commit adds the clara compiler partial imple-
    mentation with its tests. The compiler at this
    handles simple conditional statement. More complex
    handling will be added in the next commit.

---
+++ clara/sys/ccc/Condition.py
@@ -0,0 +1,157 @@
+# coding=utf-8
+
+import re
+
+from clara.base.error.ClaraException import ClaraException
+from clara.sys.ccc.ServiceState import ServiceState
+from clara.sys.ccc.Constants import Regex
+
+
+class Condition(object):
+    
+    _CONDITION_ERROR = "syntax error: malformed conditional statement"
+
+    def __init__(self, condition_string, service_name):
+        self._service_name = service_name
+        self.and_states = set()
+        self.and_not_states = set()
+        self.or_states = set()
+        self.or_not_states = set()
+        self._process(condition_string)
+
+    def _process(self, condition_string):
+        if condition_string.find("(") != -1:
+            condition_string.replace("\\C", "")
+
+        if condition_string.find(")") != -1:
+            condition_string.replace("\\)", "")
+
+        if condition_string.find("&&") != -1:
+            self._parse_condition(condition_string, "&&")
+
+        elif condition_string.find("!!") != -1:
+            self._parse_condition(condition_string, "!!")
+
+        else:
+            self._parse_condition(condition_string, None)
+
+    def _parse_condition(self, condition_string, operator):
+        if operator:
+            if condition_string.find("&&") != -1 and\
+               condition_string.find("!!") == -1:
+                iterator = iter(condition_string.split(operator))
+                while True:
+                    try:
+                        ac = iterator.next()
+                        pattern = re.compile(Regex.COMPLEX_CONDITION)
+                        match = pattern.match(ac)
+
+                        if match:
+                            if ac.find("!=") != -1:
+                                condition = ac.next()
+                                condition = condition.split("!=")
+
+                                if len(condition) != 2:
+                                    raise ClaraException(self._CONDITION_ERROR)
+
+                                stt = ServiceState(condition[0], condition[1])
+                                self.and_not_states.add(stt)
+
+                            elif ac.find("==") != -1:
+                                condition = ac.next()
+                                condition = condition.split("==")
+
+                                if len(condition) != 2:
+                                    raise ClaraException(self._CONDITION_ERROR)
+
+                                stt = ServiceState(condition[0], condition[1])
+                                self.and_states.add(stt)
+
+                            else:
+                                raise ClaraException(self._CONDITION_ERROR)
+                        else:
+                            raise ClaraException(self._CONDITION_ERROR)
+
+                    except StopIteration:
+                        break
+
+            elif (condition_string.find("!!") != -1 and
+                  condition_string.find("&&") == -1):
+                iterator = iter(condition_string.split(operator))
+
+                while True:
+                    try:
+                        ac = iterator.next()
+                        pattern = re.compile(Regex.SIMPLE_CONDITION)
+                        match = pattern.match(ac)
+
+                        if match:
+                            if ac.find("!=") != -1:
+                                condition = ac.next()
+                                condition = condition.split("!=")
+
+                                if len(condition) != 2:
+                                    raise ClaraException(self._CONDITION_ERROR)
+                                self.or_not_states.add(ServiceState(condition[0],
+                                                                    condition[1]))
+
+                            elif ac.find("==") != -1:
+                                condition = ac.next()
+                                condition = condition.split("==")
+
+                                if len(condition) != 2:
+                                    raise ClaraException(self._CONDITION_ERROR)
+                                self.or_states.add(ServiceState(condition[0],
+                                                                condition[1]))
+                            else:
+                                raise ClaraException(self._CONDITION_ERROR)
+                        else:
+                            raise ClaraException(self._CONDITION_ERROR)
+                    except StopIteration:
+                        break
+
+            else:
+                raise ClaraException(self._CONDITION_ERROR)
+
+        else:
+            pattern = re.compile(Regex.SIMPLE_CONDITION)
+            match = pattern.match(condition_string)
+
+            if match:
+                if condition_string.find("!=") != -1:
+                    condition = condition_string.split("!=")
+
+                    if len(condition) != 2:
+                        raise ClaraException(self._CONDITION_ERROR)
+                    self.or_not_states.add(ServiceState(condition[0],
+                                                        condition[1]))
+
+                elif condition_string.find("==") != -1:
+                    condition = condition_string.split("==")
+                    if len(condition) != 2:
+                        raise ClaraException(self._CONDITION_ERROR)
+                    self.or_states.add(ServiceState(condition[0],
+                                                    condition[1]))
+
+                else:
+                    raise ClaraException(self._CONDITION_ERROR)
+            else:
+                raise ClaraException(self._CONDITION_ERROR)
+
+    def is_true(self, owner_ss, input_ss):
+        c1 = Condition._check_and_condition(self.and_states, owner_ss,
+                                            input_ss)
+        c2 = Condition._check_and_condition(self.and_not_states, owner_ss,
+                                            input_ss)
+        c3 = Condition._check_or_condition(self.or_states, owner_ss, input_ss)
+        c4 = Condition._check_or_condition(self.or_not_states, owner_ss,
+                                           input_ss)
+        return c1 and c2 and c3 and c4
+
+    @staticmethod
+    def _check_and_condition(ss_set, ss1, ss2):
+        return ((ss1 in ss_set) and (ss2 in ss_set)) or not bool(len(ss_set))
+
+    @staticmethod
+    def _check_or_condition(ss_set, ss1, ss2):
+        return (ss1 in ss_set) or (ss2 in ss_set) or not bool(len(ss_set))
---
+++ test/sys/ccc/test_CCompiler.py
@@ -0,0 +1,89 @@
+# coding=utf-8
+
+import unittest
+
+from clara.base.error.ClaraException import ClaraException
+from clara.sys.ccc.CCompiler import CCompiler
+from clara.sys.ccc.ServiceState import ServiceState
+
+
+class TestCCompiler(unittest.TestCase):
+
+    @staticmethod
+    def get_composition():
+        return "10.10.10.1_java:C:S1+10.10.10.1_java:C:S2+" +\
+               "10.10.10.1_java:C:S3+10.10.10.1_java:C:S4;"
+    
+    def test_catch_missing_statement_with_exception(self):
+        with self.assertRaises(ClaraException):
+            cc = CCompiler("10.10.10.1_java:C:S5")
+            cc.compile(TestCCompiler.get_composition())
+
+    def test_service_at_the_middle(self):
+        cc = CCompiler("10.10.10.1_java:C:S3")
+        cc.compile(TestCCompiler.get_composition())
+        self.assertEqual(set(["10.10.10.1_java:C:S4"]),
+                         cc.get_unconditional_links())
+
+    def test_service_at_the_end(self):
+        cc = CCompiler("10.10.10.1_java:C:S4")
+        cc.compile(TestCCompiler.get_composition())
+        self.assertEqual(set([]), cc.get_unconditional_links())
+
+    def test_service_at_the_beginning(self):
+        cc = CCompiler("10.10.10.1_java:C:S1")
+        cc.compile(TestCCompiler.get_composition())
+        self.assertEqual(set(["10.10.10.1_java:C:S2"]),
+                         cc.get_unconditional_links())
+
+    def test_service_on_a_loop(self):
+        composition = "10.10.10.1_java:C:S1+10.10.10.1_java:C:S2+" +\
+                      "10.10.10.1_java:C:S1;"
+        cc = CCompiler("10.10.10.1_java:C:S2")
+        cc.compile(composition)
+        self.assertEqual(set(["10.10.10.1_java:C:S1"]),
+                         cc.get_unconditional_links())
+
+    def test_on_multiple_call(self):
+        composition_extra = "10.10.10.1_java:C:S1+10.10.10.1_java:C:S2+" + \
+                            "10.10.10.1_java:C:S1;"
+
+        cc = CCompiler("10.10.10.1_java:C:S2")
+        cc.compile(TestCCompiler.get_composition())
+        cc.compile(composition_extra)
+        self.assertEqual(set(["10.10.10.1_java:C:S1"]),
+                         cc.get_unconditional_links())
+
+    def test_conditional(self):
+        cc = CCompiler("10.10.10.1_java:C:S1")
+        cc.compile(TestCCompiler.get_composition())
+        composition2 = "10.10.10.1_java:C:S1;" +\
+                       "if (10.10.10.1_java:C:S1 == \"FOO\") { " +\
+                       "  10.10.10.1_java:C:S1+10.10.10.1_java:C:S2;" +\
+                       "}"
+        cc.compile(composition2)
+        owner_ss = ServiceState("10.10.10.1_java:C:S1", "\"FOO\"")
+        input_ss = ServiceState("WHATEVER", "DON'T CARE")
+        self.assertEqual(set(["10.10.10.1_java:C:S2"]), cc.get_links(owner_ss,
+                                                                     input_ss))
+
+    def test_elif_conditional(self):
+        cc = CCompiler("10.10.10.1_java:C:S1")
+        cc.compile(TestCCompiler.get_composition())
+        composition2 = "10.10.10.1_java:C:S1;" +\
+                       "if (10.10.10.1_java:C:S1 == \"FOO\") { " +\
+                       "  10.10.10.1_java:C:S1+10.10.10.1_java:C:S2;" +\
+                       "} elseif (10.10.10.1_java:C:S1 == \"BAR\") { " +\
+                       "  10.10.10.1_java:C:S1+10.10.10.1_java:C:S3;" +\
+                       "} elseif (10.10.10.1_java:C:S1 == \"FROZ\") { " +\
+                       "  10.10.10.1_java:C:S1+10.10.10.1_java:C:S4;" +\
+                       "}"
+        cc.compile(composition2)
+        owner_ss = ServiceState("10.10.10.1_java:C:S1", "\"FROZ\"")
+        input_ss = ServiceState("WHATEVER", "DON'T CARE")
+        self.assertEqual(set(["10.10.10.1_java:C:S4"]), cc.get_links(owner_ss,
+                                                                     input_ss))
+
+
+if __name__ == "__main__":
+    unittest.main()
