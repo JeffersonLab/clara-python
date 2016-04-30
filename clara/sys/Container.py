192e00815c284c38ff4b443b9474120649657dde
Ricardo Oyarzun <royarzun@gmail.com>
2016-04-30 12:34:33 -0300

    clara.sys.Container: make private what should be private

--- clara/sys/Container.py
+++ clara/sys/Container.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 from xmsg.net.xMsgAddress import ProxyAddress, RegAddress
+
 from clara.base.ClaraBase import ClaraBase
 from clara.base.ClaraNames import ServiceName
 from clara.sys.Service import Service
@@ -16,24 +17,24 @@ class Container(ClaraBase):
                                         frontend_address.host,
                                         local_address.pub_port,
                                         frontend_address.port)
-        self.container_name = container_name
-        self.logger = ClaraLogger(repr(self))
-        self.logger.log_info("container deployed")
+        self._container_name = container_name
+        self._logger = ClaraLogger(repr(self))
+        self._logger.log_info("container deployed")
 
     def __repr__(self):
         return str("Container:%s" % self.myname)
 
     def exit(self):
-        self.__remove_services()
-        self.logger.log_info("container stopped")
+        self._remove_services()
+        self._logger.log_info("container stopped")
 
     def add_service(self, engine_name, engine_class, service_pool_size,
                     initial_state):
-        service_name = ServiceName(self.container_name, engine_name)
+        service_name = ServiceName(self._container_name, engine_name)
 
         if service_name.canonical_name() in self.my_services:
-            self.logger.log_warning("Service %s already exists. No new "
-                                    "service is deployed" % str(service_name))
+            self._logger.log_warning("Service %s already exists. No new "
+                                     "service is deployed" % str(service_name))
         else:
             try:
                 service = Service(service_name,
@@ -46,7 +47,7 @@ class Container(ClaraBase):
                 self.my_services[service_name.canonical_name()] = service
 
             except Exception as e:
-                self.logger.log_exception("%s: %s" % (str(service_name), e))
+                self._logger.log_exception("%s: %s" % (str(service_name), e))
                 raise e
 
     def remove_service(self, service_name):
@@ -54,6 +55,6 @@ class Container(ClaraBase):
             service = self.my_services.pop(service_name)
             service.exit()
 
-    def __remove_services(self):
+    def _remove_services(self):
         for service in self.my_services:
             service.exit()
