263698b108aa57e8266b05f9c7010da4b92261bd
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-19 19:27:06 -0300

    Refactor ServiceReport

--- clara/util/reports/ServiceReport.py
+++ clara/util/reports/ServiceReport.py
@@ -9,95 +9,67 @@ from clara.util.reports.BaseReport import BaseReport
 
 class ServiceReport(BaseReport):
 
-    engine_name = xMsgConstants.UNDEFINED
-    class_name = xMsgConstants.UNDEFINED
-    failure_count = xMsgConstants.UNDEFINED
-    shm_reads = xMsgConstants.UNDEFINED
-    shm_writes = xMsgConstants.UNDEFINED
-    bytes_received = xMsgConstants.UNDEFINED
-    bytes_sent = xMsgConstants.UNDEFINED
-    execution_time = xMsgConstants.UNDEFINED
-    version = xMsgConstants.UNDEFINED
+    _engine_name = xMsgConstants.UNDEFINED
+    _class_name = xMsgConstants.UNDEFINED
+    _version = xMsgConstants.UNDEFINED
+
+    _failure_count = 0
+    _shm_reads = _shm_writes = 0
+    _bytes_received = _bytes_sent = 0
+    _execution_time = 0
 
     def __init__(self, service, engine):
         super(ServiceReport, self).__init__(service.myname,
                                             engine.get_author(),
                                             engine.get_description())
-        self.engine_name = engine.myname
-        self.class_name = service.get_engine_name()
-        self.version = engine.get_version
-
-    @property
-    def engine_name(self):
-        return self.engine_name
-
-    @engine_name.setter
-    def engine_name(self, engine_name):
-        self.engine_name = engine_name
-
-    @property
-    def class_name(self):
-        return self.class_name
-
-    @class_name.setter
-    def class_name(self, class_name):
-        self.class_name = class_name
+        self._engine_name = engine.myname
+        self._class_name = service.get_engine_name()
+        self._version = engine.get_version
 
     @property
     def failure_count(self):
-        return self.failure_count
+        return self._failure_count
 
-    @failure_count.setter
-    def failure_count(self, failure_count):
-        self.failure_count = failure_count
+    def increment_failure_count(self):
+        self._failure_count += 1
 
     @property
     def shm_reads(self):
-        return self.shm_reads
+        return self._shm_reads
 
-    @shm_reads.setter
-    def shm_reads(self, shm_reads):
-        self.shm_reads = shm_reads
+    def increment_shm_reads(self):
+        self._shm_reads += 1
 
     @property
     def shm_writes(self):
-        return self.shm_writes
+        return self._shm_writes
 
-    @shm_writes.setter
-    def shm_writes(self, shm_writes):
-        self.shm_writes = shm_writes
+    def increment_shm_writes(self):
+        self._shm_writes += 1
 
     @property
     def bytes_received(self):
-        return self.bytes_received
+        return self._bytes_received
 
     @bytes_received.setter
     def bytes_received(self, bytes_received):
-        self.bytes_received = bytes_received
+        self._bytes_received += bytes_received
 
     @property
     def bytes_sent(self):
-        return self.bytes_sent
+        return self._bytes_sent
 
     @bytes_sent.setter
     def bytes_sent(self, bytes_sent):
-        self.bytes_sent = bytes_sent
+        self._bytes_sent += bytes_sent
 
     @property
     def execution_time(self):
-        return self.execution_time
+        return self._execution_time
 
     @execution_time.setter
     def execution_time(self, execution_time):
-        self.execution_time = execution_time
-
-    @property
-    def version(self):
-        return self.version
-
-    @version.setter
-    def version(self, v):
-        self.version = v
+        self._execution_time += execution_time
 
     def to_json(self):
         return json.dumps(self.__dict__, sort_keys=True)
