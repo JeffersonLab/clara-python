dc1ec755629eebe658a244594f95b27b9de5be72
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-19 21:12:44 -0300

    Refactor BaseReport for cool JSON conversion

--- clara/util/reports/BaseReport.py
+++ clara/util/reports/BaseReport.py
@@ -14,67 +14,24 @@ class BaseReport(object):
     description = xMsgConstants.UNDEFINED
     start_time = xMsgConstants.UNDEFINED
     snapshot_time = xMsgConstants.UNDEFINED
-    requests_count = xMsgConstants.UNDEFINED
+    requests_count = 0
+
+    _properties = ["snapshot_time"]
 
     def __init__(self, name, author, description=""):
         self.name = name
         self.author = author
-        self.lang = ClaraLang.PYTHON
+        self.language = str(ClaraLang.PYTHON)
         self.description = description
         self.start_time = xMsgUtil.current_time()
+        self.snapshot_time = xMsgUtil.current_time()
 
-    @property
-    def name(self):
-        return self.name
+    def as_dict(self):
+        custom_dict = self.__dict__
+        for item in custom_dict:
+            if item in self._properties:
+                custom_dict[item] = str(xMsgUtil.current_time())
+        return custom_dict
 
-    @name.setter
-    def name(self, name):
-        self.name = name
-
-    @property
-    def lang(self):
-        return self.lang
-
-    @lang.setter
-    def lang(self, lang):
-        self.lang = lang
-
-    @property
-    def author(self):
-        return self.author
-
-    @author.setter
-    def author(self, author):
-        self.author = author
-
-    @property
-    def description(self):
-        return self.description
-
-    @description.setter
-    def description(self, description):
-        self.description = description
-
-    @property
-    def start_time(self):
-        return self.start_time
-
-    @start_time.setter
-    def start_time(self, start_time):
-        self.start_time = start_time
-
-    @property
-    def snapshot_time(self):
-        return self.snapshot_time
-
-    @snapshot_time.setter
-    def snapshot_time(self, snapshot_time):
-        self.snapshot_time = snapshot_time
-
-    @property
-    def requests_count(self):
-        return self.snapshot_time
-
-    @requests_count.setter
-    def requests_count(self, requests_count):
-        self._requests_count = requests_count
+    def increment_requests_count(self):
+        self.requests_count += 1
